b4c72ee5e641206bccd3ea0e3e00ed02386af0ee
VIVATOM1 <RAIVIVEKKUMAR64@GMAIL.COM>
2026-09-25 20:35:04 +0530

    Time: 8 ms (15.07%), Space: 21.9 MB (12.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,13 +3,10 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        //case 1
         if(nums[0] != 0)return 0;
-        //case 2 
         if(nums[n-1] != n)return n;
         for(int i =1;i<nums.size();i++){
             if(nums[i] != i){
-            //case 3
             return i;
             }
         }
