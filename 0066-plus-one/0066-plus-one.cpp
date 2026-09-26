0a9dfef2ee3fb0e0e1f054105483c50cf5451046
VIVATOM1 <RAIVIVEKKUMAR64@GMAIL.COM>
2026-09-26 19:28:23 +0530

    Time: 0 ms (100%), Space: 11.6 MB (60.1%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size() - 1;
+
+        while(i >= 0) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+            i--;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};/*class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i,j,len=digits.size(),a=0,next;
+        string st;
+        vector<int> b;
+        for(i=0;i<len;i++)
+        {
+            a=a*10+digits[i];
+        }
+        next=a+1;
+        if(next==10)
+        return {1,0};
+        for(j=0;j<len;j++)
+        {
+           st=to_string(next);
+           b.push_back(st[j] - '0');
+        }
+        return b;
+        
+    }
+};*/
